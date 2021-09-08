ab9678f256e22036a70577bffa8b290fb9e8784b
Andrés <Andresblancocontact@gmail.com>
2021-09-08 10:20:08 -0500

    Agregar función de bienvenida

--- toolbox.py
+++ toolbox.py
@@ -1,3 +1,10 @@
+import time
+
+
+
+#PARA IMPIMIR DE COLORES SE HACE ASÍ, PARA CAMBIAR COLOR CAMBIAS EL DONDE DICE MORADO, EL ULTIMO ES PARA QUE SE REINCIE Y NO IMPRIMA MORADO DE AHÍ EN ADELANTE
+#print(f"{bcolors.MORADO}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+
 class bcolors:  # ESTE BLOQUE DEFINE LOS COLORES
     MORADO = '\033[95m'
     BLUE = '\033[94m'
@@ -10,16 +17,32 @@ class bcolors:  # ESTE BLOQUE DEFINE LOS COLORES
     ENDC = '\033[0m'
 
 
-#PARA IMPIMIR DE COLORES SE HACE ASÍ, PARA CAMBIAR COLOR CAMBIAS EL DONDE DICE MORADO, EL ULTIMO ES PARA QUE SE REINCIE Y NO IMPRIMA MORADO DE AHÍ EN ADELANTE
-#print(f"{bcolors.MORADO}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+def welcome():  #FUNCIÓN DE BIENVENIDA
+    for i in range(10):
+        print()
+    
+    print("Iniciando sistema",end="")
+    for i in range(5):
+        time.sleep(1)
+        print(".",end="")
 
-#PARA IMPIMIR DE COLORES SE HACE ASÍ, PARA CAMBIAR COLOR CAMBIAS EL DONDE DICE MORADO, EL ULTIMO ES PARA QUE SE REINCIE Y NO IMPRIMA MORADO DE AHÍ EN ADELANTE
-#print(f"{bcolors.MORADO}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+    for i in range(3):
+        print()
+    
+    print(f"{bcolors.FAIL}$$\      $$\$$$$$$$$\$$\      $$$$$$\  $$$$$$\ $$\      $$\$$$$$$$$\ ")
+    time.sleep(0.2)
+    print(f"{bcolors.MORADO}$$ | $\  $$ $$  _____$$ |    $$  __$$\$$  __$$\$$$\    $$$ $$  _____|")
+    time.sleep(0.2)
+    print(f"{bcolors.BLUE}$$ |$$$\ $$ $$ |     $$ |    $$ /  \__$$ /  $$ $$$$\  $$$$ $$ |      ")
+    time.sleep(0.2)
+    print(f"{bcolors.WARNING}$$ $$ $$\$$ $$$$$\   $$ |    $$ |     $$ |  $$ $$\$$\$$ $$ $$$$$\    ")
+    time.sleep(0.2)
+    print(f"{bcolors.WARNING}$$$$  _$$$$ $$  __|  $$ |    $$ |     $$ |  $$ $$ \$$$  $$ $$  __|   ")
+    time.sleep(0.2)
+    print(f"{bcolors.BLUE}$$$  / \$$$ $$ |     $$ |    $$ |  $$\$$ |  $$ $$ |\$  /$$ $$ |      ")
+    time.sleep(0.2)
+    print(f"{bcolors.MORADO}$$  /   \$$ $$$$$$$$\$$$$$$$$\$$$$$$  |$$$$$$  $$ | \_/ $$ $$$$$$$$\ ")
+    time.sleep(0.2)
+    print(f"{bcolors.FAIL}\__/     \__\________\________\______/ \______/\__|     \__\________|")
 
-#PARA IMPIMIR DE COLORES SE HACE ASÍ, PARA CAMBIAR COLOR CAMBIAS EL DONDE DICE MORADO, EL ULTIMO ES PARA QUE SE REINCIE Y NO IMPRIMA MORADO DE AHÍ EN ADELANTE
-#print(f"{bcolors.MORADO}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
 
-#PARA IMPIMIR DE COLORES SE HACE ASÍ, PARA CAMBIAR COLOR CAMBIAS EL DONDE DICE MORADO, EL ULTIMO ES PARA QUE SE REINCIE Y NO IMPRIMA MORADO DE AHÍ EN ADELANTE
-#print(f"{bcolors.MORADO}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
-
-#Comentario de Jorge
